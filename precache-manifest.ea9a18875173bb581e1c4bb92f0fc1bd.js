bcd336817944652ea2c992a7e4cd1e852772ca2e
Ankita-Upadhyay <ankitaupadhyay66@gmail.com>
2018-12-30 16:52:27 -0800

    Updates

---
+++ precache-manifest.ea9a18875173bb581e1c4bb92f0fc1bd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "e0975327967fcbf7a5ad",
+    "url": "/csportfolio/static/js/runtime~main.e0975327.js"
+  },
+  {
+    "revision": "af637e7074dd1188cfe2",
+    "url": "/csportfolio/static/js/main.af637e70.chunk.js"
+  },
+  {
+    "revision": "02637f74f32e4057e7b3",
+    "url": "/csportfolio/static/js/1.02637f74.chunk.js"
+  },
+  {
+    "revision": "af637e7074dd1188cfe2",
+    "url": "/csportfolio/static/css/main.b2f9bee6.chunk.css"
+  },
+  {
+    "revision": "02637f74f32e4057e7b3",
+    "url": "/csportfolio/static/css/1.ce2db166.chunk.css"
+  },
+  {
+    "revision": "1e25b305b51c759b2630392051b4e3ed",
+    "url": "/csportfolio/index.html"
+  }
+];
